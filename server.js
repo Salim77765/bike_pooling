0ab54dfc453350d37da8532444c96e2c5093b74c
Salim <rehaanprince22222@gmail.com>
2024-12-11 02:15:29 +0530

    Initial commit

--- server.js
+++ server.js
@@ -29,9 +29,14 @@ app.use('/api/notifications', notificationsRoutes);
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+} else {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
